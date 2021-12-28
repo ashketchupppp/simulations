47292136ba6e516112af5c3d7c7d1c9640e4aba3
ashketchupppp <computerchris101@gmail.com>
2021-12-28 13:14:36 +0000

    added some line components

---
+++ src/components/curved-line.jsx
@@ -0,0 +1,31 @@
+import { CustomPIXIComponent } from "react-pixi-fiber";
+import * as PIXI from "pixi.js";
+
+const TYPE = "Line";
+export const behavior = {
+  customDisplayObject: (props) => new PIXI.Graphics(),
+  customApplyProps: function (instance, oldProps, newProps) {
+    if (
+      oldProps.x !== newProps.x ||
+      oldProps.y !== newProps.y ||
+      oldProps.endY !== newProps.endY ||
+      oldProps.endX !== newProps.endX ||
+      oldProps.controlY !== newProps.controlY ||
+      oldProps.controlX !== newProps.controlX ||
+      oldProps.lineColour !== newProps.lineColour || 
+      oldProps.width !== newProps.width 
+    ) {
+      instance.clear();
+      instance.lineStyle(newProps.width, newProps.lineColour)
+      instance.moveTo(newProps.x, newProps.y)
+      instance.quadraticCurveTo(
+        newProps.controlX,
+        newProps.controlY,
+        newProps.endX,
+        newProps.endY,
+      )
+      instance.endFill();
+    }
+  }
+};
+export default CustomPIXIComponent(behavior, TYPE)
