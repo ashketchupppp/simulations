08defca4cd95b0de9e2612add5acc359ef23604e
ashketchupppp <computerchris101@gmail.com>
2021-12-28 14:46:55 +0000

    initial

--- src/components/fluid-sim.jsx
+++ src/components/fluid-sim.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Simulation from "./simulation.jsx";
 
 export default class FluidSim extends React.Component {
   constructor (props) {
@@ -7,12 +8,43 @@ export default class FluidSim extends React.Component {
       width,
       height
     } = this.props
+
+    this.state = {
+      currentMask: []
+    }
+
+    this.nextMask = []
+    this.running = false
+  }
+
+  onRerender() {
+
+  }
+
+  onStart() {
+
+  }
+
+  onStop() {
+
+  }
+
+  onReset() {
+
   }
 
   render() {
     return (
-      <>
-      </>
+      <Simulation
+        width={this.props.width}
+        height={this.props.height}
+        onRerender={() => this.onRerender()}
+        onReset={() => this.onReset()}
+        onStart={() => this.onStart()}
+        onStop={() => this.onStop()}
+      >
+
+      </Simulation>
     )
   }
 }
